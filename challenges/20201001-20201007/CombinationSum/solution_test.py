3600afdfbed1e691817972cab5680453de0c5b8a
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-10-05 13:53:24 +0300

    .

--- challenges/20201001-20201007/CombinationSum/solution_test.py
+++ challenges/20201001-20201007/CombinationSum/solution_test.py
@@ -1,5 +1,7 @@
 import unittest
 
+from solution import Solution
+
 
 class SolutionTest(unittest.TestCase):
     def setUp(self):
@@ -8,19 +10,24 @@ class SolutionTest(unittest.TestCase):
     def test_1(self):
         actual_result = self.solution.combinationSum(candidates=[2, 3, 6, 7], target=8)
         print(actual_result)
+        self.assertEquals([[2,2,3],[7]], actual_result)
     
     def test_2(self):
         actual_result = self.solution.combinationSum(candidates=[2, 3, 5], target=8)
         print(actual_result)
+        self.assertEquals([[2,2,2,2],[2,3,3],[3,5]], actual_result)
 
     def test_3(self):
         actual_result = self.solution.combinationSum(candidates=[2], target=1)
         print(actual_result)
+        self.assertEquals([], actual_result)
     
     def test_4(self):
         actual_result = self.solution.combinationSum(candidates=[1], target=1)
         print(actual_result)
+        self.assertEquals([[1]], actual_result)
     
     def test_5(self):
         actual_result = self.solution.combinationSum(candidates=[1], target=2)
         print(actual_result)
+        self.assertEquals([[1, 1]], actual_result)
