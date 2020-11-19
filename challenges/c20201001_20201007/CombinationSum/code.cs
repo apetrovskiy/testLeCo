21039364a3be0d100512547b278869d4ad325308
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-11-19 12:04:42 +0300

    .

--- challenges/c20201001_20201007/CombinationSum/code.cs
+++ challenges/c20201001_20201007/CombinationSum/code.cs
@@ -1,5 +1,6 @@
+using System.Collections.Generic;
 public class Solution {
     public IList<IList<int>> CombinationSum(int[] candidates, int target) {
-        
+        return null;
     }
 }
---
+++ src/test/challenges/c20201001_20201007/SomeTest.cs
@@ -0,0 +1,10 @@
+namespace challenges.c20201001_20201007{
+    using NUnit.Framework;
+    public class SomeTest{
+        [Test]
+        public void Test() {
+            // TODO: make it really working
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
