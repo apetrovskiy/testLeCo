755f37c59289a0b34db8f3034883abf426b02378
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-21 16:09:41 +0300

    ts, js

--- src/test/java/problems/medium/GasStation/GasStationTest.java
+++ src/test/java/problems/medium/GasStation/GasStationTest.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-//import problems.medium.GasStation.SolutionSc;
+// import problems.medium.GasStation.SolutionSc;
 
 class GasStationTest {
     @ParameterizedTest
