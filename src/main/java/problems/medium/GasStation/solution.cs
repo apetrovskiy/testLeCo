42dd396c550bc914741f15e9decb3fa09113d1ba
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-21 05:37:01 +0300

    C# is done

--- src/main/java/problems/medium/GasStation/solution.cs
+++ src/main/java/problems/medium/GasStation/solution.cs
@@ -1,10 +1,30 @@
 namespace problems.medium.GasStation
 {
+    using System.Linq;
+
     public class Solution
     {
         public int CanCompleteCircuit(int[] gas, int[] cost)
         {
-            return 0;
+            var differences = Enumerable.Range(0, gas.Length)
+            .Select(index => gas[index] - cost[index])
+            .ToList();
+            if (differences.Sum() < 0)
+                return -1;
+
+            var starting = 0;
+            var tank = 0;
+            for (var index = 0; index < gas.Length; index++)
+            {
+                tank = tank + gas[index] - cost[index];
+                if (tank < 0)
+                {
+                    starting = index + 1;
+                    tank = 0;
+                }
+            }
+
+            return starting;
         }
     }
 }
