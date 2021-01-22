b6aba8ab75d574ab521c027458d08e6cf12a2800
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-22 12:16:45 +0300

    preparation

---
+++ src/test/java/problems/easy/IsSubsequence/test_is_subsequence.py
@@ -0,0 +1,13 @@
+from src.main.java.problems.easy.IsSubsequence.solution3 import Solution
+import pytest
+
+
+test_data = [
+    ("abc", "ahbgdc", True),
+    ("axc", "ahbgdc", False)
+]
+
+
+@pytest.mark.parametrize("s,t,expected_result", test_data)
+def test_is_subsequence(s: str, t: str, expected_result: bool):
+    assert expected_result == Solution().isSubsequence(s, t)
