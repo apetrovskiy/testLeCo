abcc78eb9d66b224b79a093783f13512517b531b
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-15 22:58:53 +0300

    preparation

---
+++ src/test/java/challenges/c20210101_20210107/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree/test_find_a_corresponding_node_of_a_Binary_tree_in_a_clone_of_that_tree.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+test_data = []
+
+
+@pytest.mark.parametrize("",test_data)
+def test_find_a_corresponding_node_of_a_Binary_tree_in_a_clone_of_that_tree():
+    assert expected_result==1
