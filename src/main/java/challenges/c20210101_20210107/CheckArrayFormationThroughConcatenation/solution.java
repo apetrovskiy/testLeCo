ffedcfc69c36072e425233e7fca70b01a53172b1
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-15 23:11:54 +0300

    preparation

--- src/main/java/challenges/c20210101_20210107/CheckArrayFormationThroughConcatenation/solution.java
+++ src/main/java/challenges/c20210101_20210107/CheckArrayFormationThroughConcatenation/solution.java
@@ -1,3 +1,5 @@
+package challenges.c20210101_20210107.CheckArrayFormationThroughConcatenation;
+
 class Solution {
     public boolean canFormArray(int[] arr, int[][] pieces) {
         return false;
