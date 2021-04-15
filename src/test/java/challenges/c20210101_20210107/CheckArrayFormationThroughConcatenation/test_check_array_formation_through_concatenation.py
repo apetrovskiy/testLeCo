5af2f595e87b24115e2fb3dd37b27c487615adf9
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-15 22:42:55 +0300

    preparation

---
+++ src/test/java/challenges/c20210101_20210107/CheckArrayFormationThroughConcatenation/test_check_array_formation_through_concatenation.py
@@ -0,0 +1,18 @@
+from src.main.java.challenges.c20210101_20210107.CheckArrayFormationThroughConcatenation.solution import Solution
+import pytest
+from typing import List
+from src.main.java.challenges.c20210101_20210107.CheckArrayFormationThroughConcatenation.solution import Solution
+
+
+test_data = [
+    ([85], [[85]], True),
+    ([15, 88], [[88], [15]], True),
+    ([49, 18, 16], [[16, 18, 49]], False),
+    ([91, 4, 64, 78], [[78], [4, 64], [91]], True),
+    ([1, 3, 5, 7], [[2, 4, 6, 8]], False)
+]
+
+
+@pytest.mark.parametrize("input_array,pieces,expected_result", test_data)
+def test_check_array_formation_through_concatenation(input_array: List[int], pieces: List[List[int]], expected_result: bool):
+    assert expected_result == Solution().canFormArray(input_array, pieces)
