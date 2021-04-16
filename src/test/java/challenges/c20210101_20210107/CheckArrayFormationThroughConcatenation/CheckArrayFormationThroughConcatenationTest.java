098a9780d40b2f6632f7569ed7e27bda10162ba6
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-16 07:16:08 +0300

    Update CheckArrayFormationThroughConcatenationTest.java

--- src/test/java/challenges/c20210101_20210107/CheckArrayFormationThroughConcatenation/CheckArrayFormationThroughConcatenationTest.java
+++ src/test/java/challenges/c20210101_20210107/CheckArrayFormationThroughConcatenation/CheckArrayFormationThroughConcatenationTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.of;
 
 import java.util.stream.Stream;
-import static org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
