46ac153ffd3aa1db80531361b2340b96d58a30b8
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-05-07 23:32:09 +0300

    flake8

--- src/main/java/problems/hard/MaximumPerformanceOfATeam/maximum_performance_of_a_team.py
+++ src/main/java/problems/hard/MaximumPerformanceOfATeam/maximum_performance_of_a_team.py
@@ -2,5 +2,7 @@ from typing import List
 
 
 class Solution:
-    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+    def maxPerformance(
+            self, n: int, speed: List[int],
+            efficiency: List[int], k: int) -> int:
         return 0
