de8dd8099d557889801bf087b82c1812c8474918
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-05-07 16:58:53 +0300

    ktlint

--- src/main/java/challenges/c20201001_20201007/CombinationSum/solution.kt
+++ src/main/java/challenges/c20201001_20201007/CombinationSum/solution.kt
@@ -1,5 +1,11 @@
-class Solution {
+package challenges.c20201001_20201007.CombinationSum
+
+class SolutionKt {
     fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
+        // for ktlint only
+        listOf(candidates).forEach { println(it) }
+        println(target)
+
         return listOf()
     }
 }
--- src/main/java/problems/medium/GasStation/solution.kt
+++ src/main/java/problems/medium/GasStation/solution.kt
@@ -3,17 +3,15 @@ package problems.medium.GasStation
 class SolutionKt {
     fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
         val differences = IntRange(0, gas.size - 1)
-        .map {index -> gas[index] - cost[index]}
+            .map { index -> gas[index] - cost[index] }
         if (differences.sum() < 0)
             return -1
 
         var starting = 0
         var tank = 0
-        for (index in 0..gas.size - 1)
-        {
+        for (index in 0..gas.size - 1) {
             tank = tank + gas[index] - cost[index]
-            if (tank < 0)
-            {
+            if (tank < 0) {
                 starting = index + 1
                 tank = 0
             }
