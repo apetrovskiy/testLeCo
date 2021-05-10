314fbc4fdeba2654c16304d66cf2b0b30c4c93be
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-05-10 16:19:03 +0300

    Update greatest_common_divisor_of_strings.py

--- src/main/java/problems/easy/GreatestCommonDivisorOfStrings/greatest_common_divisor_of_strings.py
+++ src/main/java/problems/easy/GreatestCommonDivisorOfStrings/greatest_common_divisor_of_strings.py
@@ -1,13 +1,20 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        print(f"str1 = {str1}, str2 = {str2}")
-        if len(str1) < len(str2):
-            if str1 in str2:
-                return str1
-        else:
-            if str2 in str1:
-                return str2
-        return "-"
+        result: str = ""
+        max_divisor_length = min(len(str1), len(str2))
+        current_divisor: str = ""
+        for i in range(0, max_divisor_length):
+            current_divisor += str1[i]
+            if self.is_divisor_of(current_divisor, str1) and self.is_divisor_of(current_divisor, str2):
+                result = current_divisor
+        return result
 
-    def is_divisor_of(str1: str, str2: str) -> bool:
-        return False
+    def is_divisor_of(self, divisor: str, data: str) -> bool:
+        result: bool = False
+        if len(divisor) > len(data):
+            return result
+        if divisor == data:
+            return True
+        list_from_str2 = data.split(divisor)
+        result = all(x == '' for x in list_from_str2)
+        return result
