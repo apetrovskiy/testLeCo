58651771b64897b18554e83af6c50d07db2f890b
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-05-10 13:55:23 +0300

    preparation

---
+++ src/main/java/problems/easy/GreatestCommonDivisorOfStrings/greatest_common_divisor_of_strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        print(f"str1 = {str1}, str2 = {str2}")
+        if len(str1) < len(str2):
+            if str1 in str2:
+                return str1
+        else:
+            if str2 in str1:
+                return str2
+        return "-"
+
+    def is_divisor_of(str1: str, str2: str) -> bool:
+        
