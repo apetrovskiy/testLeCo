fc6cd0c9086705ca0e81cec14b134d0f9eb942d7
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-05-10 20:17:46 +0300

    Update GreatestCommonDivisorOfStrings.kt

--- src/main/java/problems/easy/GreatestCommonDivisorOfStrings/GreatestCommonDivisorOfStrings.kt
+++ src/main/java/problems/easy/GreatestCommonDivisorOfStrings/GreatestCommonDivisorOfStrings.kt
@@ -2,6 +2,25 @@ package problems.easy.GreatestCommonDivisorOfStrings
 
 class SolutionKt {
     fun gcdOfStrings(str1: String, str2: String): String {
-        return ""
+        var result: String = ""
+        var maxDivisorLength: Int = listOf(str1.length, str2.length).min()!!
+        var currentDivisor: String = ""
+        for (i in 1..maxDivisorLength) {
+            currentDivisor += str1[i]
+            if (isDivisorOf(currentDivisor, str1) && isDivisorOf(currentDivisor, str2))
+                result = currentDivisor
+        }
+        return result
+    }
+
+    fun isDivisorOf(divisor: String, data: String): Boolean {
+        var result: Boolean = false
+        if (divisor.length > data.length)
+            return result
+        if (divisor == data)
+            return true
+        val listOfDataParth = data.split(divisor)
+        result = listOfDataParth.all { it == "" }
+        return result
     }
 }
