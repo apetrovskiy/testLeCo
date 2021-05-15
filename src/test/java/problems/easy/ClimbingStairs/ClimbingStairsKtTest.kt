69ee26f4025fc732d582af33dae9e0f713de483e
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-05-16 00:44:13 +0300

    Climbing Stairs is done (Kotlin, Scala)

--- src/test/java/problems/easy/ClimbingStairs/ClimbingStairsKtTest.kt
+++ src/test/java/problems/easy/ClimbingStairs/ClimbingStairsKtTest.kt
@@ -1,1 +1,23 @@
 package problems.easy.ClimbingStairs
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+class ClimbingStairsKtTest {
+    @ParameterizedTest
+    @MethodSource("getInputData")
+    fun testClimbingStairs(input: Int, expectedResult: Int) {
+        assertEquals(expectedResult, SolutionKt().climbStairs(input))
+    }
+
+    companion object {
+        @JvmStatic
+        fun getInputData(): Stream<Arguments> {
+            return Stream.of(
+                of(1, 1), of(2, 2), of(3, 3), of(4, 5), of(5, 8)
+            )
+        }
+    }
+}
--- src/test/java/problems/easy/ClimbingStairs/ClimbingStairsScalaTest.scala
+++ src/test/java/problems/easy/ClimbingStairs/ClimbingStairsScalaTest.scala
@@ -1,1 +1,26 @@
 package problems.easy.ClimbingStairs
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+class ClimbingStairsScalaTest {
+  @ParameterizedTest
+  @MethodSource(Array("getInputData"))
+  def testClimbingStairs(input: Int, expectedResult: Int) = {
+    assertEquals(expectedResult, SolutionScala.climbStairs(input))
+  }
+}
+
+object ClimbingStairsScalaTest {
+  def getInputData(): Stream[Arguments] = {
+    Stream.of(
+      of(1, 1),
+      of(2, 2),
+      of(3, 3),
+      of(4, 5),
+      of(5, 8)
+    )
+  }
+}
