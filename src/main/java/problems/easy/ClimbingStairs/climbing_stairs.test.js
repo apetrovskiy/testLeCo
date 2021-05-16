2172dfecfa23e0af6f8e50234dad188403978730
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-05-16 03:56:33 +0300

    Climbing Stairs is done (JS)

---
+++ src/main/java/problems/easy/ClimbingStairs/climbing_stairs.test.js
@@ -0,0 +1,13 @@
+const { climbStairs } = require('./climbing_stairs');
+
+describe.each([
+    [1, 1],
+    [2, 2],
+    [3, 3],
+    [4, 5],
+    [5, 8],
+])("climbStairs(%i) should equal %i", (input, expectedResult) => {
+    test(`input = {input}, returns = {expectedResult}`, () => {
+        expect(climbStairs(input)).toBe(expectedResult);
+    });
+});
