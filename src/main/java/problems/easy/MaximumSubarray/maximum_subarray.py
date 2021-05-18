fb4bcd6f7b5b0b8e985bb41174433bc3e798b7e2
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-05-18 16:08:49 +0300

    Maximum Subarray (Python), allure-commandline

--- src/main/java/problems/easy/MaximumSubarray/maximum_subarray.py
+++ src/main/java/problems/easy/MaximumSubarray/maximum_subarray.py
@@ -3,4 +3,12 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        return 0
+        current_max = -2_000_000_000
+        current_max_plus_next = 0
+        for i in range(len(nums)):
+            current_max_plus_next += nums[i]
+            if current_max < current_max_plus_next:
+                current_max = current_max_plus_next
+            if current_max_plus_next < 0:
+                current_max_plus_next = 0
+        return current_max
