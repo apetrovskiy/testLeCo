b09145497abcf73d0e73132de3b9130fdad99b60
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-05-19 22:25:09 +0300

    Maximum Subarray is done (JS)

--- src/main/java/problems/easy/MaximumSubarray/maximum_subarray.test.js
+++ src/main/java/problems/easy/MaximumSubarray/maximum_subarray.test.js
@@ -1,7 +1,32 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maxSubArray = function(nums) {
-    return 0;
-};
+const { maxSubArray } = require("./maximum_subarray");
+
+describe.each([
+    [
+        [-2, 1, -3, 4, -1, 2, 1, -5, 4], 6
+    ],
+    [
+        [1], 1
+    ],
+    [
+        [5, 4, -1, 7, 8], 23
+    ],
+    [
+        [5, -3, 6, -7, 6], 8
+    ],
+    [
+        [6, -7, 6, -3, 5], 8
+    ],
+    [
+        [-2, -3, 4, -1, -2, 1, 5, -3], 7
+    ],
+    [
+        [-1], -1
+    ],
+    [
+        [-2, -1], -1
+    ],
+])("maxSubArray(%o) should equal %i", (inputArray, expectedResult) => {
+    test(`inputArray = {inputArray}, returns {expectedResult}`, () => {
+        expect(maxSubArray(inputArray)).toBe(expectedResult);
+    });
+});
