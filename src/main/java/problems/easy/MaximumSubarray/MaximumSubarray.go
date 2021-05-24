087ee7199be8f720554dc8fcb4ad16bafe80f3e5
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-05-24 15:04:51 +0300

    experimental changes

--- src/main/java/problems/easy/MaximumSubarray/MaximumSubarray.go
+++ src/main/java/problems/easy/MaximumSubarray/MaximumSubarray.go
@@ -1,6 +1,6 @@
 package MaximumSubarray
 
-func maxSubArray(nums []int) int {
+func MaxSubArray(nums []int) int {
 	currentMax := -2_000_000_000
 	currentMaxPlusNext := 0
 	for i := 0; i < len(nums); i++ {
--- src/main/java/problems/easy/MaximumSubarray/MaximumSubarray_test.go
+++ src/main/java/problems/easy/MaximumSubarray/MaximumSubarray_test.go
@@ -9,15 +9,18 @@ import (
 	"testing"
 )
 
-/* var _ = Describe("CountBits()", func() {
+var _ = Describe("CountBits()", func() {
 	It("basic tests", func() {
-		Expect(CountBits(0)).To(Equal(0))
-		Expect(CountBits(4)).To(Equal(1))
-		Expect(CountBits(7)).To(Equal(3))
-		Expect(CountBits(9)).To(Equal(2))
-		Expect(CountBits(10)).To(Equal(2))
+		Expect(MaxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})).To(Equal(6))
+		Expect(MaxSubArray([]int{1})).To(Equal(1))
+		Expect(MaxSubArray([]int{5, 4, -1, 7, 8})).To(Equal(23))
+		Expect(MaxSubArray([]int{5, -3, 6, -7, 6})).To(Equal(8))
+		Expect(MaxSubArray([]int{6, -7, 6, -3, 5})).To(Equal(8))
+		Expect(MaxSubArray([]int{-2, -3, 4, -1, -2, 1, 5, -3})).To(Equal(7))
+		Expect(MaxSubArray([]int{-1})).To(Equal(1))
+		Expect(MaxSubArray([]int{-2, -1})).To(Equal(1))
 	})
-}) */
+})
 
 // https://towardsdatascience.com/use-environment-variable-in-your-next-golang-project-39e17c3aaa66
 // use godot package to load/read the .env file and
@@ -35,20 +38,20 @@ func goDotEnvVariable(key string) string {
 }
 
 func dotest(input []int, exp int) {
-	var ans = maxSubArray(input)
+	var ans = MaxSubArray(input)
 	Expect(ans).To(Equal(exp))
 }
 
 func TestStepBitCounting(t *testing.T) {
 	dotenv := goDotEnvVariable("ALLURE_RESULTS_PATH")
 	allure.Test(t, allure.Action(func() {
-		Expect(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})).To(Equal(6))
-		Expect(maxSubArray([]int{1})).To(Equal(1))
-		Expect(maxSubArray([]int{5, 4, -1, 7, 8})).To(Equal(23))
-		Expect(maxSubArray([]int{5, -3, 6, -7, 6})).To(Equal(8))
-		Expect(maxSubArray([]int{6, -7, 6, -3, 5})).To(Equal(8))
-		Expect(maxSubArray([]int{-2, -3, 4, -1, -2, 1, 5, -3})).To(Equal(7))
-		Expect(maxSubArray([]int{-1})).To(Equal(1))
-		Expect(maxSubArray([]int{-2, -1})).To(Equal(1))
+		Expect(MaxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})).To(Equal(6))
+		Expect(MaxSubArray([]int{1})).To(Equal(1))
+		Expect(MaxSubArray([]int{5, 4, -1, 7, 8})).To(Equal(23))
+		Expect(MaxSubArray([]int{5, -3, 6, -7, 6})).To(Equal(8))
+		Expect(MaxSubArray([]int{6, -7, 6, -3, 5})).To(Equal(8))
+		Expect(MaxSubArray([]int{-2, -3, 4, -1, -2, 1, 5, -3})).To(Equal(7))
+		Expect(MaxSubArray([]int{-1})).To(Equal(1))
+		Expect(MaxSubArray([]int{-2, -1})).To(Equal(1))
 	}))
 }
