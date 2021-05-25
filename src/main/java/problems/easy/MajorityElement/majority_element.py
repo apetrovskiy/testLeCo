39511486cf37c492455faa522a134f0baf8452a6
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-05-25 22:17:14 +0300

    Majority Element is done (Python)

--- src/main/java/problems/easy/MajorityElement/majority_element.py
+++ src/main/java/problems/easy/MajorityElement/majority_element.py
@@ -6,12 +6,8 @@ class Solution:
         min_number_needed = len(nums) // 2 + 1
         results = {}
         for i in range(len(nums)):
-            if i in results.keys():
-                results[i] += 1
+            if nums[i] in results.keys():
+                results[nums[i]] += 1
             else:
-                results[i] = 1
-        # return max([x[0] for x in results if x[1] >= min_number_needed])
-        for i in range(len(results)):
-            if i.value >= min_number_needed:
-                return i.key
-        return 0
+                results[nums[i]] = 1
+        return [k for (k, v) in results.items() if v >= min_number_needed][0]
