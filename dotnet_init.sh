4db39eee3b6eb0486c64e91d687c10b2f8c494bf
Alexander Petrovskiy <petrovskiy.alexander@gmail.com>
2023-06-30 19:24:34 +0300

    finished

--- dotnet_init.sh
+++ dotnet_init.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-rm -f src/test/test.csproj
+rm allure-results/* -y
+
+MAIN_PRJ_FILE=src/main/main.csproj
+TEST_PRJ_FILE=src/test/test.csproj
+TEST_PRJ_TMP_FILE=src/test/test.tmp
+
+rm -f "${TEST_PRJ_FILE}"
 rm -f src/test/Class1.cs
-rm -f src/main/main.csproj
+rm -f "${MAIN_PRJ_FILE}"
 rm -f src/main/Class1.cs
 rm -f testLeCo.sln
 
 dotnet new sln --name testLeCo
 dotnet new classlib --name main --framework net7.0 --output src/main
 dotnet new classlib --name test --framework net7.0 --output src/test
-dotnet sln add src/main/main.csproj
-dotnet sln add src/test/test.csproj
-dotnet add src/test/test.csproj reference src/main/main.csproj
+dotnet sln add "${MAIN_PRJ_FILE}"
+dotnet sln add "${TEST_PRJ_FILE}"
+dotnet add "${TEST_PRJ_FILE}" reference "${MAIN_PRJ_FILE}"
 
 rm -f src/test/Class1.cs
 rm -f src/main/Class1.cs
@@ -28,3 +34,22 @@ dotnet add src/test package NUnit3TestAdapter
 dotnet add src/test package NUnit.Allure
 dotnet add src/test package NUnit.Allure.Steps
 dotnet add src/test package Allure.Commons
+dotnet add src/test package NUnit.Analyzers
+
+export PROJECT_TAG="</Project>"
+export ITEM_GROUP="\n  <ItemGroup>\n    <Content Include=\"allureConfig.json\">\n      <CopyToOutputDirectory>Always</CopyToOutputDirectory>\n    </Content>\n  </ItemGroup>\n\n</Project>"
+
+echo "============================="
+echo "${ITEM_GROUP}"
+echo "============================="
+echo "${PROJECT_TAG}"
+
+sed '$d' "${TEST_PRJ_FILE}"
+tac "${TEST_PRJ_FILE}" | tail -n +3 | tac >"${TEST_PRJ_TMP_FILE}"
+echo "${ITEM_GROUP}" >>"${TEST_PRJ_TMP_FILE}"
+mv "${TEST_PRJ_TMP_FILE}" "${TEST_PRJ_FILE}"
+
+dotnet clean
+dotnet restore
+dotnet build
+dotnet test
