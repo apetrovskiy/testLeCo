6b06c8ec20580c1505af8bd8bac87030df1a19f0
Alexander Petrovskiy <petrovskiy.alexander@gmail.com>
2023-07-25 01:07:23 +0300

    experimental fixes

--- src/main/java/problems/easy/MergeStringsAlternately/MergeStringsAlternatelySolution.cs
+++ src/main/java/problems/easy/MergeStringsAlternately/MergeStringsAlternatelySolution.cs
@@ -7,10 +7,23 @@ public class MergeStringsAlternatelySolution
         // string addSpaces(string w1, int number) => w1.PadRight(number);
 
         var diff = word1.Length - word2.Length;
-        if (diff > 0) word2 = word2.PadRight(diff, ' ');
-        if (diff < 0) word1 = word1.PadRight(-diff, ' ');
+
+        Console.WriteLine($"diff == {diff}");
+
+        if (diff > 0) word2 = word2.PadRight(word2.Length + diff);
+        if (diff < 0) word1 = word1.PadRight(word1.Length - diff);
+
+        Console.WriteLine($"word1: '{word1}', word2: '{word2}'");
 
         var collection = Enumerable.Range(0, word1.Length).Select(index => $"{word1[index]}{word2[index]}").ToList();
-        return string.Concat(collection);
+        // return string.Concat(collection);
+
+        var result = string.Concat(collection);
+
+        Console.WriteLine($"result == '{result}'");
+
+        return result;
+
+
     }
 }
