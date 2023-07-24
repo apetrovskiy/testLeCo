fb810aad3941a07221fe7c62917224a232cb3972
Alexander Petrovskiy <petrovskiy.alexander@gmail.com>
2023-07-24 23:58:47 +0300

    fixes

--- src/main/java/problems/easy/MergeStringsAlternately/MergeStringsAlternatelySolution.cs
+++ src/main/java/problems/easy/MergeStringsAlternately/MergeStringsAlternatelySolution.cs
@@ -10,7 +10,7 @@ public class MergeStringsAlternatelySolution
         if (diff > 0) word2 = word2.PadRight(diff, ' ');
         if (diff < 0) word1 = word1.PadRight(-diff, ' ');
 
-        var collection = Enumerable.Range(0, word1.Length - 1).Select(index => $"{word1[index]}{word2[index]}").ToList();
+        var collection = Enumerable.Range(0, word1.Length).Select(index => $"{word1[index]}{word2[index]}").ToList();
         return string.Concat(collection);
     }
 }
