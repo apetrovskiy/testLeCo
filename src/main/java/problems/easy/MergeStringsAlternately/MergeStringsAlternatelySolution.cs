c81a680f9e2a0f04ae4b9207cf67ec6aec7d476e
Alexander Petrovskiy <petrovskiy.alexander@gmail.com>
2023-07-24 23:43:04 +0300

    the first version

--- src/main/java/problems/easy/MergeStringsAlternately/MergeStringsAlternatelySolution.cs
+++ src/main/java/problems/easy/MergeStringsAlternately/MergeStringsAlternatelySolution.cs
@@ -7,8 +7,8 @@ public class MergeStringsAlternatelySolution
         // string addSpaces(string w1, int number) => w1.PadRight(number);
 
         var diff = word1.Length - word2.Length;
-        if (diff > 0) word2 = word2.PadRight(diff);
-        if (diff < 0) word1 = word1.PadRight(diff);
+        if (diff > 0) word2 = word2.PadRight(diff, ' ');
+        if (diff < 0) word1 = word1.PadRight(-diff, ' ');
 
         var collection = Enumerable.Range(0, word1.Length - 1).Select(index => $"{word1[index]}{word2[index]}").ToList();
         return string.Concat(collection);
