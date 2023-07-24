e6f317534fd5c7ef42e02854e52ab45373d8b525
Alexander Petrovskiy <petrovskiy.alexander@gmail.com>
2023-07-25 01:39:52 +0300

    finished

--- src/main/java/problems/easy/MergeStringsAlternately/MergeStringsAlternatelySolution.cs
+++ src/main/java/problems/easy/MergeStringsAlternately/MergeStringsAlternatelySolution.cs
@@ -4,26 +4,15 @@ public class MergeStringsAlternatelySolution
     public string MergeAlternately(string word1, string word2)
     {
         if (string.IsNullOrEmpty(word1) && string.IsNullOrEmpty(word2)) return "";
-        // string addSpaces(string w1, int number) => w1.PadRight(number);
 
         var diff = word1.Length - word2.Length;
+        var commonLength = word1.Length > word2.Length ? word2.Length : word1.Length;
 
-        Console.WriteLine($"diff == {diff}");
+        var tail = "";
+        if (diff > 0) tail = word1[commonLength..];
+        if (diff < 0) tail = word2[commonLength..];
 
-        if (diff > 0) word2 = word2.PadRight(word2.Length + diff);
-        if (diff < 0) word1 = word1.PadRight(word1.Length - diff);
-
-        Console.WriteLine($"word1: '{word1}', word2: '{word2}'");
-
-        var collection = Enumerable.Range(0, word1.Length).Select(index => $"{word1[index]}{word2[index]}").ToList();
-        // return string.Concat(collection);
-
-        var result = string.Concat(collection);
-
-        Console.WriteLine($"result == '{result}'");
-
-        return result;
-
-
+        var collection = Enumerable.Range(0, commonLength).Select(index => $"{word1[index]}{word2[index]}").ToList();
+        return string.Concat(collection) + tail;
     }
 }
